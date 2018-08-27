71064dda08d703bd480b0205e4b7b65455723b9b
John Kennedy <john_kennedy@shaw.ca>
2018-08-26 20:55:01 -0600

    Removing references to Adoption.sol
    Removing extended references from Adoption.sol

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,9 @@
-var Adoption = artifacts.require("./Adoption.sol");
 var Store = artifacts.require("./Store.sol");
 var StoreOwner = artifacts.require("./StoreOwner.sol");
 var Administrator = artifacts.require("./Administrator.sol");
 var Product = artifacts.require("./Product.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Adoption);
   deployer.deploy(Store);
   deployer.deploy(StoreOwner);
   deployer.deploy(Administrator);
